a7e5ffe1fb19a3a682410ef4b277c9002b14b5d2
Frantisek Lachman <flachman@redhat.com>
2020-07-03 10:09:33 +0200

    Update help message
    Signed-off-by: Frantisek Lachman <flachman@redhat.com>

--- dist2src/cli.py
+++ dist2src/cli.py
@@ -54,7 +54,7 @@ def cli(verbose):
         $ dist2src extract-archive rpms/rpm src/rpm
         $ dist2src copy-spec rpms/rpm src/rpm
         $ dist2src add-packit-config src/rpm
-        $ dist2src copy-patches rpms/rpm src/rpm
+        $ dist2src copy-all-sources rpms/rpm src/rpm
         $ dist2src apply-patches src/rpm
     """
     logger.addHandler(logging.StreamHandler())
