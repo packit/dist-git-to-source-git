1c19ff87817f95e2995688fbd9f274297423c035
Jiri Popelka <jpopelka@redhat.com>
2020-09-26 12:13:46 +0200

    [deployment] env-config ConfigMap

--- dist2src/worker/tasks.py
+++ dist2src/worker/tasks.py
@@ -7,9 +7,7 @@ from typing import Optional
 from dist2src.worker.celerizer import celery_app
 from dist2src.worker.processor import Processor
 
-task_name = getenv("CELERY_TASK_NAME", "task.dist2src.process_message")
 
-
-@celery_app.task(name=task_name)
+@celery_app.task(name=getenv("CELERY_TASK_NAME"))
 def process_message(event: dict, **kwargs) -> Optional[dict]:
     return Processor().process_message(event=event)
